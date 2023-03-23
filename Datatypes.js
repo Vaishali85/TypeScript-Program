7778b4df8c0a3b0b19753596cd1bd9ad11f8c395
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-03-23 20:23:37 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,20 @@
+// [Varible    Variable Name : Datatype = Value ;]
+//Number
+var no1 = 100.45;
+console.log(no1);
+//String
+var course1 = "Full Stack Developer";
+console.log(course1);
+//Array                   1)number  2)String
+var array = [10, 20, 30, 40, 50];
+console.log(array);
+var string = ['html', 'css', 'bootstrap', 'js'];
+console.log(string);
+//tuple
+var course2 = ['html', 'js'];
+console.log(course2);
+//any
+var x;
+20, 30, 50;
+;
+console.log(x);
---
+++ Datatypes.ts
@@ -0,0 +1,25 @@
+// [Varible    Variable Name : Datatype = Value ;]
+//Number
+var no1:number=100.45;
+console.log(no1);
+
+
+//String
+let course1:string="Full Stack Developer";
+console.log(course1);
+
+
+//Array                   1)number  2)String
+let array:number[]=[10,20,30,40,50];
+console.log(array);
+
+let string:string[]=['html','css','bootstrap','js'];
+console.log(string);
+
+//tuple
+let course2:tuple[]=['html','js'];
+console.log(course2);
+
+//any
+var x:any=[10,20,30,50];
+console.log(x);
