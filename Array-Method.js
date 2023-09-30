229922684fa0649a973cd5ae50e482f344333781
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-09-30 20:31:35 +0530

    Add files via upload

--- Array-Method.js
+++ Array-Method.js
@@ -1,2 +1,48 @@
-var Day = "Have A Nice Day";
-console.log(Day.substr(5, 5));
+/***********************pop**************************** */
+//remove element form last
+var city = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+console.log(city.pop());
+/***************************push*********************** */
+//add element in last
+var city1 = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+console.log(city1.push('Chennai'));
+console.log(city1);
+/********************************Shift******************** */
+//remove element from begining
+var city2 = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+console.log(city2.shift());
+console.log(city2);
+/********************************UnShift******************** */
+//add element from begining
+var city3 = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+console.log(city3.unshift('Chennai'));
+console.log(city3);
+/********************************reverse******************** */
+//reverse the array
+var city4 = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+console.log(city4.reverse());
+/********************************sort******************** */
+//sorting array
+var city5 = ['Pune', 'Mumabai', 'Delhi', 'Kolkatta'];
+console.log(city5.sort());
+/********************************includes******************** */
+//let city6:String[]=['Pune','mumbai','Delhi','Kolkatta'];
+//console.log(city6.includes('Pune'));
+/********************************indexOf******************** */
+var city7 = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+console.log(city7.indexOf('Mumbai'));
+/********************************lastindexOf******************** */
+var city8 = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+console.log(city8.lastIndexOf('Pune'));
+/********************************concat******************** */
+var city9 = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+var city10 = ['Chennai', 'Jaipur'];
+console.log(city9.concat(city10));
+/********************************length******************** */
+console.log(city10.length);
+/********************************splice******************** */
+var city11 = ['Pune', 'Mumbai', 'Delhi', 'Kolkatta'];
+console.log(city11.splice(1, 5));
+/********************************toString******************** */
+var city12 = ['Pune', 'Mumabai', 'Delhi', 'Kolkatta'];
+console.log(city12.toString());
--- Array-Method.ts
+++ Array-Method.ts
@@ -1,2 +1,70 @@
-let Day:String="Have A Nice Day";
-console.log(Day.substr(5,5));
+
+/***********************pop**************************** */
+//remove element form last
+let city:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+console.log(city.pop());
+
+/***************************push*********************** */
+//add element in last
+let city1:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+console.log(city1.push('Chennai'));
+console.log(city1);
+
+/********************************Shift******************** */
+//remove element from begining
+let city2:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+console.log(city2.shift());
+console.log(city2);
+
+/********************************UnShift******************** */
+//add element from begining
+let city3:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+console.log(city3.unshift('Chennai'));
+console.log(city3);
+
+/********************************reverse******************** */
+//reverse the array
+let city4:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+console.log(city4.reverse());
+
+/********************************sort******************** */
+//sorting array
+let city5:String[]=['Pune','Mumabai','Delhi','Kolkatta'];
+console.log(city5.sort());
+
+/********************************includes******************** */
+
+//let city6:String[]=['Pune','mumbai','Delhi','Kolkatta'];
+//console.log(city6.includes('Pune'));
+
+/********************************indexOf******************** */
+
+let city7:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+console.log(city7.indexOf('Mumbai'));
+
+/********************************lastindexOf******************** */
+
+let city8:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+console.log(city8.lastIndexOf('Pune'));
+
+/********************************concat******************** */
+
+let city9:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+let city10:String[]=['Chennai','Jaipur'];
+console.log(city9.concat(city10));
+
+/********************************length******************** */
+
+console.log(city10.length);
+
+/********************************splice******************** */
+
+let city11:String[]=['Pune','Mumbai','Delhi','Kolkatta'];
+console.log(city11.splice(1,5));
+
+/********************************toString******************** */
+
+let city12:String[]=['Pune','Mumabai','Delhi','Kolkatta'];
+console.log(city12.toString());
+
+/********************************Join******************** */
